8dadb5c501b47786a0a948196e6f104ceab6172e
José Manuel Nieto <superjmn@outlook.com>
2023-08-14 18:40:27 +0200

    Add ReactiveCommandMixin

---
+++ src/Zafiro.UI/ReactiveCommandMixin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+using ReactiveUI;
+
+namespace AvaloniaSyncer.ViewModels;
+
+public static class ReactiveCommandMixin
+{
+    public static ReactiveCommand<TInput, TResult> Extend<TInput, TResult>(this ReactiveCommand<TInput, TResult> command, Action<TResult> afterExecution)
+    {
+        return ReactiveCommand.CreateFromTask<TInput, TResult>(async input =>
+        {
+            var firstAsync = await command.Execute(input).FirstAsync();
+            afterExecution(firstAsync);
+            return firstAsync;
+        }, command.CanExecute);
+    }
+}
