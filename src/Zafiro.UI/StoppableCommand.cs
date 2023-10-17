5493aacaf5ddcafbc8d9a5808253b6960945f490
José Manuel Nieto <superjmn@outlook.com>
2023-10-17 16:50:52 +0200

    Improve StoppableCommand (#2)

--- src/Zafiro.UI/StoppableCommand.cs
+++ src/Zafiro.UI/StoppableCommand.cs
@@ -1,26 +1,26 @@
 ﻿using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Windows.Input;
+using CSharpFunctionalExtensions;
 using ReactiveUI;
 
 namespace Zafiro.UI;
 
 public static class StoppableCommand
 {
-    public static StoppableCommand<TIn, TOut> Create<TIn, TOut>(Func<TIn, IObservable<TOut>> logic, IObservable<bool> canStart)
+    public static StoppableCommand<TIn, TOut> Create<TIn, TOut>(Func<TIn, IObservable<TOut>> logic, Maybe<IObservable<bool>> canStart)
     {
         return new StoppableCommand<TIn, TOut>(logic, canStart);
     }
 
-    public static StoppableCommand<Unit, TOut> Create<TOut>(Func<IObservable<TOut>> logic, IObservable<bool> canStart)
+    public static StoppableCommand<Unit, TOut> Create<TOut>(Func<IObservable<TOut>> logic, Maybe<IObservable<bool>> canStart)
     {
         return new StoppableCommand<Unit, TOut>(_ => logic(), canStart);
     }
 
-    public static IStoppableCommand<Unit, TOut> CreateFromTask<TOut>(Func<CancellationToken, Task<TOut>> task, IObservable<bool> canExecute)
+    public static IStoppableCommand<Unit, TOut> CreateFromTask<TOut>(Func<CancellationToken, Task<TOut>> task, Maybe<IObservable<bool>> canStart)
     {
-        return new StoppableCommand<Unit, TOut>(_ => Observable.FromAsync(task), canExecute);
+        return new StoppableCommand<Unit, TOut>(_ => Observable.FromAsync(task), canStart);
     }
 }
 
@@ -28,12 +28,12 @@ public class StoppableCommand<TIn, TOut> : IStoppableCommand<TIn, TOut>, IStoppa
 {
     private readonly ReactiveCommand<TIn, TOut> startCommand;
 
-    public StoppableCommand(Func<TIn, IObservable<TOut>> logic, IObservable<bool> canStart)
+    public StoppableCommand(Func<TIn, IObservable<TOut>> logic, Maybe<IObservable<bool>> canStart)
     {
         var isExecuting = new Subject<bool>();
         var stopCommand = ReactiveCommand.Create(() => { }, isExecuting);
         Stop = stopCommand;
-        startCommand = ReactiveCommand.CreateFromObservable<TIn, TOut>(e => logic(e).TakeUntil(stopCommand), canStart);
+        startCommand = ReactiveCommand.CreateFromObservable<TIn, TOut>(e => logic(e).TakeUntil(stopCommand), canStart.GetValueOrDefault());
         Start = startCommand;
         startCommand.IsExecuting.Subscribe(isExecuting);
     }
