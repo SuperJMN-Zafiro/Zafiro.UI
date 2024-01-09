ab220cc8a327a9a7956948769eead58e23ec1b78
José Manuel Nieto <superjmn@outlook.com>
2024-01-09 16:12:11 +0100

    Cleanup

--- src/Zafiro.UI/Fields/Field.cs
+++ src/Zafiro.UI/Fields/Field.cs
@@ -18,22 +18,15 @@ public class Field<T> : ReactiveValidationObject, IField
             return Unit.Default;
         }, IsValid);
         IsDirty = this.WhenAnyValue(x => x.CommittedValue, x => x.Value, (cv, v) => !Equals(cv, v));
-        Rollback = ReactiveCommand.Create(() =>
-        {
-            Value = CommittedValue!;
-        });
+        Rollback = ReactiveCommand.Create(() => { Value = CommittedValue!; });
 
         CommittedValue = initialValue;
     }
 
-    [Reactive]
-    public T CommittedValue { get; private set; }
-
-    [Reactive]
-    public T Value { get; set; }
-
-    public ReactiveCommandBase<Unit, Unit> Commit { get; }
+    [Reactive] public T CommittedValue { get; private set; }
+    [Reactive] public T Value { get; set; }
     public ReactiveCommandBase<Unit, Unit> Rollback { get; }
+    public ReactiveCommandBase<Unit, Unit> Commit { get; }
     public IObservable<bool> IsValid => ValidationContext.Valid;
     public IObservable<bool> IsDirty { get; }
 }
