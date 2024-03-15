e91b422aeecd9ee2bb6a2de85359e0ef8cd6bab6
José Manuel Nieto <superjmn@outlook.com>
2024-03-15 22:18:27 +0100

    Add Section (#6)
    * Return to stream
    
    * Section
    
    * Restore

---
+++ src/Zafiro.UI/Models/ISection.cs
@@ -0,0 +1,8 @@
+﻿namespace Zafiro.Avalonia.Models;
+
+public interface ISection
+{
+    public string Title { get; }
+    public object Icon { get; }
+    public object Content { get; }
+}
---
+++ src/Zafiro.UI/Models/Section.cs
@@ -0,0 +1,16 @@
+﻿namespace Zafiro.Avalonia.Models;
+
+public class Section : ISection
+{
+    public Section(string title, object content, object icon)
+    {
+        Title = title;
+        Content = content;
+        Icon = icon;
+    }
+
+    public string Title { get; }
+    public object Icon { get; }
+
+    public object Content { get; }
+}
