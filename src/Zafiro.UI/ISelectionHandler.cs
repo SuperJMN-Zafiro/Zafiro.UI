4e0e2a10cac034e87aaf1fe25f06d386392bda41
José Manuel Nieto <superjmn@outlook.com>
2024-03-21 23:55:16 +0100

    Mucho que revisar

---
+++ src/Zafiro.UI/ISelectionHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Reactive;
+using DynamicData;
+using ReactiveUI;
+
+namespace Zafiro.UI;
+
+public interface ISelectionHandler
+{
+    ReactiveCommand<Unit, Unit> SelectNone { get; }
+    ReactiveCommand<Unit, Unit> SelectAll { get; }
+    IObservable<SelectionKind> SelectionKind { get; }
+}
+
+public interface ISelectionHandler<T, TKey> : ISelectionHandler where T : notnull where TKey : notnull
+{
+    IObservable<IChangeSet<T, TKey>> Changes { get; }
+}
