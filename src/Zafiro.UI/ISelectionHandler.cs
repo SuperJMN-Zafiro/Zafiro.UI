5d6959008bce380eb448bcb631b9bf224757464d
José Manuel Nieto <superjmn@outlook.com>
2024-03-22 18:10:15 +0100

    Vamos a tope

--- src/Zafiro.UI/ISelectionHandler.cs
+++ src/Zafiro.UI/ISelectionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reactive;
+using System.Reactive.Linq;
 using DynamicData;
 using ReactiveUI;
 
@@ -8,9 +9,16 @@ public interface ISelectionHandler
 {
     ReactiveCommand<Unit, Unit> SelectNone { get; }
     ReactiveCommand<Unit, Unit> SelectAll { get; }
-    IObservable<SelectionKind> SelectionKind { get; }
-    IObservable<int> SelectedItems { get; }
-    IObservable<int> TotalItems { get; }
+    IObservable<int> SelectionCount { get; }
+    IObservable<int> TotalCount { get; }
+}
+
+public static class SelectionHandlerMixin
+{
+    public static IObservable<SelectionKind> Kind(this ISelectionHandler selectionHandler)
+    {
+        return selectionHandler.SelectionCount.WithLatestFrom(selectionHandler.TotalCount, (selected, total) => selected == 0 ? UI.SelectionKind.None : total == selected ? UI.SelectionKind.Full : UI.SelectionKind.Partial);
+    }
 }
 
 public interface ISelectionHandler<T, TKey> : ISelectionHandler where T : notnull where TKey : notnull
