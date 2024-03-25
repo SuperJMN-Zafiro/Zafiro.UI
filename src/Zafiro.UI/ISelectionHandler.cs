6b3e0d3c38e4c19913eae4a2979cde538842db19
José Manuel Nieto <superjmn@outlook.com>
2024-03-25 13:15:20 +0100

    Reorg

--- src/Zafiro.UI/ISelectionHandler.cs
+++ src/Zafiro.UI/ISelectionHandler.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reactive;
-using System.Reactive.Linq;
 using DynamicData;
 using ReactiveUI;
 
@@ -11,14 +10,6 @@ public interface ISelectionHandler
     ReactiveCommand<Unit, Unit> SelectAll { get; }
     IObservable<int> SelectionCount { get; }
     IObservable<int> TotalCount { get; }
-}
-
-public static class SelectionHandlerMixin
-{
-    public static IObservable<SelectionKind> Kind(this ISelectionHandler selectionHandler)
-    {
-        return selectionHandler.SelectionCount.WithLatestFrom(selectionHandler.TotalCount, (selected, total) => selected == 0 ? UI.SelectionKind.None : total == selected ? UI.SelectionKind.Full : UI.SelectionKind.Partial);
-    }
 }
 
 public interface ISelectionHandler<T, TKey> : ISelectionHandler where T : notnull where TKey : notnull
