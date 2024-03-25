b8391135995beaa246ed52219118baf00378e8d1
José Manuel Nieto <superjmn@outlook.com>
2024-03-25 13:15:54 +0100

    Add selection abstractions (#8)

---
+++ src/Zafiro.UI/ISelectionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Reactive;
+using DynamicData;
+using ReactiveUI;
+
+namespace Zafiro.UI;
+
+public interface ISelectionHandler
+{
+    ReactiveCommand<Unit, Unit> SelectNone { get; }
+    ReactiveCommand<Unit, Unit> SelectAll { get; }
+    IObservable<int> SelectionCount { get; }
+    IObservable<int> TotalCount { get; }
+}
+
+public interface ISelectionHandler<T, TKey> : ISelectionHandler where T : notnull where TKey : notnull
+{
+    IObservable<IChangeSet<T, TKey>> SelectionChanges { get; }
+}
---
+++ src/Zafiro.UI/SelectionHandlerMixin.cs
@@ -0,0 +1,11 @@
+﻿using System.Reactive.Linq;
+
+namespace Zafiro.UI;
+
+public static class SelectionHandlerMixin
+{
+    public static IObservable<SelectionKind> Kind(this ISelectionHandler selectionHandler)
+    {
+        return selectionHandler.SelectionCount.WithLatestFrom(selectionHandler.TotalCount, (selected, total) => selected == 0 ? UI.SelectionKind.None : total == selected ? UI.SelectionKind.Full : UI.SelectionKind.Partial);
+    }
+}
