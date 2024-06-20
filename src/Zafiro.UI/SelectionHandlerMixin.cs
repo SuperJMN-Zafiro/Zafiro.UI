6c6c55ad25147f23b53778608e84a4018a2176e9
José Manuel NIeto Sánchez (SuperJMN) <superjmn@outlook.com>
2024-06-20 12:54:33 +0200

    La cosa no va mal, pero el SelectionKind me está causando un derrame

--- src/Zafiro.UI/SelectionHandlerMixin.cs
+++ src/Zafiro.UI/SelectionHandlerMixin.cs
@@ -4,8 +4,11 @@ namespace Zafiro.UI;
 
 public static class SelectionHandlerMixin
 {
-    public static IObservable<SelectionKind> Kind(this ISelectionHandler selectionHandler)
+    public static IObservable<SelectionKind> Kinds(this ISelectionHandler selectionHandler)
     {
-        return selectionHandler.SelectionCount.WithLatestFrom(selectionHandler.TotalCount, (selected, total) => selected == 0 ? UI.SelectionKind.None : total == selected ? UI.SelectionKind.Full : UI.SelectionKind.Partial);
+        selectionHandler.SelectionCount.Subscribe(i => { });
+        selectionHandler.TotalCount.Subscribe(i => { });
+        
+        return selectionHandler.SelectionCount.WithLatestFrom(selectionHandler.TotalCount.StartWith(0), (selected, total) => selected == 0 ? UI.SelectionKind.None : total == selected ? UI.SelectionKind.Full : UI.SelectionKind.Partial);
     }
 }
