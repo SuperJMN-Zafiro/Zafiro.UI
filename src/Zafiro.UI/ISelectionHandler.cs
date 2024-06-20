9159a4ca942c0986a8e8c6b216f667d70783cd81
José Manuel NIeto Sánchez (SuperJMN) <superjmn@outlook.com>
2024-06-20 13:48:48 +0200

    Selection works

--- src/Zafiro.UI/ISelectionHandler.cs
+++ src/Zafiro.UI/ISelectionHandler.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reactive;
-using DynamicData;
 using ReactiveUI;
 
 namespace Zafiro.UI;
@@ -10,9 +9,4 @@ public interface ISelectionHandler
     ReactiveCommand<Unit, Unit> SelectAll { get; }
     IObservable<int> SelectionCount { get; }
     IObservable<int> TotalCount { get; }
-}
-
-public interface ISelectionHandler<T, TKey> : ISelectionHandler where T : notnull where TKey : notnull
-{
-    IObservable<IChangeSet<T, TKey>> SelectionChanges { get; }
 }
--- src/Zafiro.UI/SelectionHandlerMixin.cs
+++ src/Zafiro.UI/SelectionHandlerMixin.cs
@@ -6,9 +6,6 @@ public static class SelectionHandlerMixin
 {
     public static IObservable<SelectionKind> Kinds(this ISelectionHandler selectionHandler)
     {
-        selectionHandler.SelectionCount.Subscribe(i => { });
-        selectionHandler.TotalCount.Subscribe(i => { });
-        
-        return selectionHandler.SelectionCount.WithLatestFrom(selectionHandler.TotalCount.StartWith(0), (selected, total) => selected == 0 ? UI.SelectionKind.None : total == selected ? UI.SelectionKind.Full : UI.SelectionKind.Partial);
+        return selectionHandler.SelectionCount.WithLatestFrom(selectionHandler.TotalCount, (selected, total) => selected == 0 ? SelectionKind.None : total == selected ? UI.SelectionKind.Full : UI.SelectionKind.Partial);
     }
 }
